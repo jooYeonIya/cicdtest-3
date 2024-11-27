3174d6d06f4ec8587d27a2dcb1783fe17f1021a5
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-27 11:54:26 +0900

    first commit

---
+++ src/main/java/org/exmple/cicdtest3/controller/MainController.java
@@ -0,0 +1,12 @@
+package org.exmple.cicdtest3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+  @GetMapping("/")
+  public String main() {
+    return "cicd test 3";
+  }
+}
